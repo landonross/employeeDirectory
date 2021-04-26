db10773baa02a60ffe1b56ff7ea25702215bb607
Landon Ross <landon.w.ross@gmail.com>
2021-04-26 11:31:32 -0600

    Base react code file with a few files added for the requested page

---
+++ employee-directory/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import MainContainer from './components/MainPage'
+import './components/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainContainer />
+      <Footer />  
+    </div>
+  );
+}
+
+export default App;
