08bf6c5390af64dde7c564d9a2fc74acb02dc4a1
Landon Ross <landon.w.ross@gmail.com>
2021-04-26 13:15:22 -0600

    added footer and header js code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MainContainer from './components/MainPage'
-import './components/index.css'
+import './index.css'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MainContainer />
+      {/* <MainContainer /> */}
       <Footer />  
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+function Navbar(){
+    return (
+        <nav>
+            <h1>Employee Directory</h1>
+        </nav>
+    )
+}
+
+export default Navbar
